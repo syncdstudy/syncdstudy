cd969b4561d226f599df65fbec0387c0bdb5ca2f
Angela Joy Almeron <aalmeron@hawaii.edu>
2025-04-15 13:54:48 -1000

    Update seed.ts

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-await-in-loop */
 import { PrismaClient, Role, Condition } from '@prisma/client';
 import { hash } from 'bcrypt';
 import * as config from '../config/settings.development.json';
@@ -7,8 +8,8 @@ const prisma = new PrismaClient();
 async function main() {
   console.log('Seeding the database');
   const password = await hash('changeme', 10);
-  config.defaultAccounts.forEach(async (account) => {
-    const role = account.role as Role || Role.USER;
+  for (const account of config.defaultAccounts) {
+    const role = (account.role as Role) || Role.USER;
     console.log(`  Creating user: ${account.email} with role: ${role}`);
     await prisma.user.upsert({
       where: { email: account.email },
@@ -19,8 +20,7 @@ async function main() {
         role,
       },
     });
-    // console.log(`  Created user: ${user.email} with role: ${user.role}`);
-  });
+  }
   for (const data of config.defaultData) {
     const condition = data.condition as Condition || Condition.good;
     console.log(`  Adding stuff: ${JSON.stringify(data)}`);
